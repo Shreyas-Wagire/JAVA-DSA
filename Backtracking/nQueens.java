ddc0db1c9e82240b0042aaeaa355520a3ee7274b
Shreyas-Wagire <shreyaswagire07sw@gmail.com>
2025-01-05 15:12:32 +0530

    backtracking added

---
+++ Backtracking/nQueens.java
@@ -0,0 +1,62 @@
+class nQueens {
+
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertical up direction
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // dig left
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // dig right
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void nQueen(char board[][], int row) {
+        if (row == board.length) {
+            printBoard(board);
+            return;
+        }
+
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueen(board, row + 1); // recursion
+                board[row][j] = 'X'; // backtracking
+            }
+        }
+    }
+
+    public static void printBoard(char board[][]) {
+        System.out.println("---------- Chess Board ---------");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.err.print((board[i][j] + " "));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        char board[][] = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X'; // initialize with empty squares
+            }
+        }
+
+        nQueen(board, 0); // Start solving the N-Queens problem
+    }
+}
